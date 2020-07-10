2c71aac2277fd8544db70ab2396a00bbeaa157c4
Thomas J. Fan <thomasjpfan@gmail.com>
2020-07-10 14:50:10 -0400

    REL Try cal version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md') as f:
     LONG_DESCRIPTION = f.read()
 
 setup(name="sksearchspace",
-      version="0.0.1.0.23.1.dev2",
+      version="2020.07.10.0.23.1.dev0",
       description="Defines search spaces for scikit-lean estimators",
       author="Thomas J. Fan",
       author_email="thomasjpfan@gmail.com",
