8f25c0e09cbcee36438c391c750287afe72cb3ee
Thomas J. Fan <thomasjpfan@gmail.com>
2020-07-09 23:02:13 -0400

    BLD Fix setup

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup, find_packages
 
+with open('README.md') as f:
+    LONG_DESCRIPTION = f.read()
+
 setup(name="sksearchspace",
       version="0.0.1.dev0",
       description="Defines search spaces for scikit-lean estimators",
       author="Thomas J. Fan",
       author_email="thomasjpfan@gmail.com",
+      long_description=LONG_DESCRIPTION,
+      long_description_content_type="text/markdown",
       url="https://github.com/thomasjpfan/sksearchspace",
       packages=find_packages(include=["sksearchspace"]),
       include_package_data=True,
