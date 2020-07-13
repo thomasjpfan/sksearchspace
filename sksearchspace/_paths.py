c2311aac7e575d1663d7f88f95b189c374667cdb
Thomas J. Fan <thomasjpfan@gmail.com>
2020-07-13 13:33:46 -0400

    ENH Adds ExtraTreeRegressor

--- sksearchspace/_paths.py
+++ sksearchspace/_paths.py
@@ -3,11 +3,15 @@ from pathlib import Path
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.tree import ExtraTreeClassifier
+from sklearn.tree import ExtraTreeRegressor
 
 HERE = Path(__file__).parent
 
 ESTIMATOR_TO_PCS_PATH = {
+
+    # tree
     DecisionTreeClassifier: HERE / "tree" / "DecisionTreeClassifier.pcs_new",
     DecisionTreeRegressor: HERE / "tree" / "DecisionTreeRegressor.pcs_new",
     ExtraTreeClassifier: HERE / "tree" / "ExtraTreeClassifier.pcs_new",
+    ExtraTreeRegressor: HERE / "tree" / "ExtraTreeRegressor.pcs_new"
 }
